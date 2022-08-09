4edff502c7816d1e67a4d9796a3929364ca95268
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-08-08 22:50:51 -0500

    set up variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,79 @@
-const startMinutes = 1.5;
-let time = startMinutes * 60;
+//Variables
 
-const countdownEl = document.getElementById('countdown');
+// timer
+let countDown = 80;
+let timer = document.getElementById("timer");
 
-setInterval(updateCountdown, 1000);
+// containers
+let scoreBox = document.getElementById("score-div");
+let questionBox = document.getElementById("question-div");
+let choicesBox = document.getElementById("multiple-choice");
+let results = document.getElementById("results");
+let buttonBox = document.getElementById("start-button-box");
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+// buttons and results
+let scoresBtn = document.getElementById("view-scores");
+let startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", setTime);
 
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--;
-}
+// High score storage 
+let highScoreStorage = [];
+let storedScores = JSON.parse(window.localStorage.getItem("highScores"));
+
+// Questions
+ let questionNumber = 0;
+let questions = [
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        Choices: [],
+        answer: ""
+    },
+];
+
+// Score
+let score = 0;
