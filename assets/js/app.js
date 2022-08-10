630c63b9e89532a8f3d1fd6d2ccb365413d48865
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-08-10 15:11:20 -0500

    set up questions

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,55 +24,55 @@ let storedScores = JSON.parse(window.localStorage.getItem("highScores"));
  let questionNumber = 0;
 let questions = [
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "Inside which HTML element do we put the JavaScript?",
+        Choices: ["<scripting>", "<javascript>", "<js>", "<script>"],
+        answer: "<script>"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "Where is the correct place to insert a JavaScript?",
+        Choices: ["The <body> section", "Both the <head> section and the <body> section are correct", "the <head> section"],
+        answer: "Both the <head> section and the <body> section are correct"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "The external JavaScript file must contain the <script> tag.",
+        Choices: ["True", "False"],
+        answer: "False"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "How do you write 'Hello World' in an alert box?",
+        Choices: ["alert('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "msgBox('Hello World');"],
+        answer: "alert('Hello World');"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "How do you create a function in JavaScript?",
+        Choices: ["function myFunction()", "function = myFunction()", "function: myFunction()"],
+        answer: "function myFunction()"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "How do you call a function named 'myFunction'?",
+        Choices: ["call myFunction()", "call function myFunction()", "myFunction()"],
+        answer: "myFunction()"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "How can you add a comment in a JavaScript?",
+        Choices: ["'This is a comment'", "<!--This is a comment-->", "//This is a comment"],
+        answer: "// This is a comment"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "How to insert a comment that has more than one line?",
+        Choices: ["/* this comment has more than one line */", "<!-- This comment has more than one line -->","// This comment has more than one line //"],
+        answer: "/* this comment has more than one line */"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
+        title: "Which event occurs when the user clicks on an HTML element?",
+        Choices: ["onclick", "onmouseclick", "onmouseover", "onchange"],
+        answer: "onclick"
     },
     {
-        title: "",
-        Choices: [],
-        answer: ""
-    },
+        title: "JavaScript is the same as Java.",
+        Choices: ["True", "False"],
+        answer: "False"
+    }
 ];
 
 // Score
